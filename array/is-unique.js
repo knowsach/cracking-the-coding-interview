715320319684faf4abf000a362629ba278d6c810
sachin <knowsach@gmail.com>
2020-07-18 01:22:11 +0530

    unique string algo implemented

--- array/is-unique.js
+++ array/is-unique.js
@@ -1,1 +1,18 @@
 // determine whether string has all unique characters
+
+let str = 'my mstrisng'
+
+function isUnique(str) {
+
+    let unique = true;
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j < str.length; j++) {
+            if (str[i] == str[j])    //if matches
+                unique = false;
+        }
+    }
+
+    console.log('unique:', unique);
+}
+
+isUnique(str);
